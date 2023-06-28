282fb2c1345bd84710f9bed1a3eff71ca9bfe68e
Victor Sampaio <victor.sampaio2003@gmail.com>
2023-06-28 15:31:28 -0300

    commit 12

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ console.log("esse console foi feito no meu serviço via cloud")
 //Alteração local feita no serviço
 console.log("esse console foi feito no meu serviço localmente")
 //Alteração cloud 2 feita no serviço
+console.log("essa foi a segunda alteracao feita via cloud no meu serviço")
