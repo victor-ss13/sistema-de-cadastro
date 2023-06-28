47b21900c95c539358e65f546626ed15ed12db1a
Victor Sampaio <victor.sampaio2003@gmail.com>
2023-06-28 15:30:54 -0300

    commit 10

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectando ao banco de dados");
 //Alteração cloud feita no serviço
 console.log("esse console foi feito no meu serviço via cloud")
-//Alteração local 
+//Alteração local feita no serviço
 console.log("esse console foi feito no meu serviço localmente")
